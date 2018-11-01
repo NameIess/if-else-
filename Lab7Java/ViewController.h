56711018418cf77711b6f4223d24812d2cebc5f7
Dima <dima@Dimas-Mac.local>
2018-11-01 18:56:10 +0300

    Initial Commit

---
+++ Lab7Java/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab7Java
+//
+//  Created by Dima  on 11/1/18.
+//  Copyright © 2018 Dima . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
